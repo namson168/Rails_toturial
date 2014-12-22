b703b82b3cbe510236656244609c638da97d4ec9
namson168 <namsonqd@gmail.com>
2014-12-22 21:53:26 +0700

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(name:  "Datbq",
-             email: "example@railstutorial.org",
+             email: "datbuiquoc1993@gmail.com",
              password:              "password",
              password_confirmation: "password",
-             admin: true)
-
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
+
 end
